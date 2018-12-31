dd628174393275377d1862a371c664efbac649ee
Tristan Smeets <tristansmeets89@hotmail.com>
2018-12-31 20:45:08 +0100

    fixing some old merge conflicts

--- mge_v18_student_version/src/mge/behaviours/CameraOrbitBehaviour.hpp
+++ mge_v18_student_version/src/mge/behaviours/CameraOrbitBehaviour.hpp
@@ -3,28 +3,18 @@
 
 #include "mge/behaviours/AbstractBehaviour.hpp"
 #include "mge/core/GameObject.hpp"
-#include <SFML/Window/Window.hpp>
 
 class CameraOrbitBehaviour : public AbstractBehaviour
 {
 public:
-	CameraOrbitBehaviour(float distance, float maxTilt, float rotationSpeed, GameObject* target, sf::Window* window);
+	CameraOrbitBehaviour(const glm::vec3& distance, float tiltInDegrees, float rotationSpeed, GameObject* target);
 	virtual ~CameraOrbitBehaviour();
 	virtual void update(float step);
 
 private:
 	float rotationSpeed;
 	GameObject* target;
-	sf::Window* window;
-	glm::vec3 cameraPosition;
-	glm::vec3 targetPosition;
-	glm::vec3 cameraForward;
-	glm::vec3 cameraSide;
-	glm::vec3 cameraUp;
-	float tiltInDegrees = 0.0f;
-	float maxTilt;
-	sf::Vector2i oldMousePos;
-	glm::vec3 rotateAroundTarget(float rotationsRadians, const glm::vec3& rotationAxis ) const;
+	glm::mat4 rotateAroundTarget(float rotationsRadians, const glm::vec3& rotationAxis ) const;
 	void mouseInput();
 };
 

f163add3c96ae34a6f834845423e5450dd18dd0b
Tristan Smeets <tristansmeets89@hotmail.com>
2019-03-01 15:49:11 +0100

    Added the UIStepCounter and added images and font for hud stepcounter.

---
+++ mge_v18_student_version/assets/LuaGameScripts/UI/HUD.lua
@@ -0,0 +1,22 @@
+--Stepcounter
+--Text
+Font = "mge/UI/Futura_ICG.ttf"			--Path to the font
+FontSize = 16							--Size of the font
+TextPosition = {x = 200, y = 1500}
+--Icon
+StepIcon = "mge/UI/FootSteps.png"       --Path to the image
+IconPosition = {x = 100, y = 1500 }		--Where Icon is supposed to be placed.
+
+
+--Hintbox
+HamsterIcon = ""                        --Path to the image.
+
+--Hint Textbox
+HintDialogue = {                        --Paths to hint image.
+    Level_1 = "",
+    level_2 = "",
+    Level_3 = "",
+    Level_4 = "",
+    Level_5 = "",
+    Level_6 = ""
+}
---
+++ mge_v18_student_version/src/mge/User Interface/UIStepCounter.cpp
@@ -0,0 +1,67 @@
+#include "UIStepCounter.hpp"
+
+UIStepCounter::UIStepCounter(lua_State* luaHUD, int levelNumber) : Observer<GeneralEvent>()
+{
+	std::cout << "Creating UI StepCounter.\n";
+	
+	//Getting amount of steps
+	lua_State* luaLevelInfo = LuaWrapper::InitializeLuaState("LuaGameScripts/Level/Level_Info.lua");
+	lua_getglobal(luaLevelInfo, "AmountOfSteps");
+	totalSteps = LuaWrapper::GetTableNumber(luaLevelInfo, "Level_" + std::to_string(levelNumber));
+	stepsLeft = totalSteps;
+
+	//Creating Icon
+	iconImage = sf::Sprite();
+	std::string iconPath = LuaWrapper::GetString(luaHUD, "StepIcon");
+	iconTexture.loadFromFile(iconPath);
+	iconImage.setTexture(iconTexture);
+	glm::vec2 iconPosition = LuaWrapper::GetVec2(luaHUD, "IconPosition");
+	iconImage.setPosition(iconPosition.x, iconPosition.y);
+	sf::Vector2u iconSize = iconImage.getTexture()->getSize();
+	iconImage.setOrigin(iconSize.x * 0.5f, iconSize.y * 0.5f);
+
+	//Creating font
+	std::string fontPath = LuaWrapper::GetString(luaHUD, "Font");
+	font.loadFromFile(fontPath);
+	
+	//Creating Text
+	stepText = sf::Text();
+	stepText.setFont(font);
+	stepText.setCharacterSize(LuaWrapper::GetNumber<int>(luaHUD, "FontSize"));
+	glm::vec2 textPosition = LuaWrapper::GetVec2(luaHUD, "TextPosition");
+	stepText.setPosition(textPosition.x, textPosition.y);
+	updateStepCounter(stepsLeft);
+}
+
+UIStepCounter::~UIStepCounter()
+{
+	std::cout << "GC running on:UIStepCounter.\n";
+}
+
+void UIStepCounter::OnNotify(const GeneralEvent & info)
+{
+	if (info.stepsTaken > 0)
+	{
+		stepsLeft -= info.stepsTaken;
+		updateStepCounter(stepsLeft);
+	}
+	if (info.resetLevel)
+		resetStepCounter();
+}
+
+void UIStepCounter::Draw(sf::RenderWindow * window)
+{
+	window->draw(stepText);
+	window->draw(iconImage);
+}
+
+void UIStepCounter::resetStepCounter()
+{
+	stepsLeft = totalSteps;
+	updateStepCounter(stepsLeft);
+}
+
+void UIStepCounter::updateStepCounter(int Number)
+{
+	stepText.setString(std::to_string(Number));
+}

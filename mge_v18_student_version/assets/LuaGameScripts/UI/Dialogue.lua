57b7a6240749d45e91ca21bbdf76322e30eab869
Tristan Smeets <tristansmeets89@hotmail.com>
2019-03-06 14:23:28 +0100

    Updated the dialogue images and updated the lua file.

--- mge_v18_student_version/assets/LuaGameScripts/UI/Dialogue.lua
+++ mge_v18_student_version/assets/LuaGameScripts/UI/Dialogue.lua
@@ -2,84 +2,56 @@
     Add the paths to the images that need to be shown to the correct table.]]
 --Level 1
 Level1_Start = {
-    "mge/UI/Dialogue/cat1.png"
+    "mge/UI/Dialogue/Lvl1_start_1.png",
+    "mge/UI/Dialogue/Lvl1_start_2.png"
 }
 
 Level1_Exit = {
-    "mge/UI/Dialogue/hamster1.png"
 }
 
 --Level 2
 Level2_Start = {
-    "mge/UI/Dialogue/cat2.png",
-    "mge/UI/Dialogue/cat1.png"
+    "mge/UI/Dialogue/Lvl2_start_1.png"
 }
 
 Level2_Exit = {
-    "mge/UI/Dialogue/hamster1.png",
-    "mge/UI/Dialogue/hamster2.png"
 }
 
 --Level 3
 Level3_Start = {
-    "mge/UI/Dialogue/cat1.png",
-	"mge/UI/Dialogue/cat2.png",
-	"mge/UI/Dialogue/cat1.png"
+    "mge/UI/Dialogue/Lvl3_start_1.png",
+    "mge/UI/Dialogue/Lvl3_start_2.png"
 }
 
 Level3_Exit = {
-    "mge/UI/Dialogue/hamster1.png",
-	"mge/UI/Dialogue/hamster2.png",
-	"mge/UI/Dialogue/hamster1.png"
 }
 
 --Level 4
 Level4_Start = {
-	"mge/UI/Dialogue/cat1.png",
-    "mge/UI/Dialogue/cat2.png",
-	"mge/UI/Dialogue/cat1.png",
-    "mge/UI/Dialogue/cat2.png"
+	"mge/UI/Dialogue/Lvl4_start_1.png",
+	"mge/UI/Dialogue/Lvl4_start_2.png"
 }
 
 Level4_Exit = {
-    "mge/UI/Dialogue/hamster1.png",
-	"mge/UI/Dialogue/hamster2.png",
-	"mge/UI/Dialogue/hamster1.png",
-	"mge/UI/Dialogue/hamster2.png"
 }
 
 --Level 5
 Level5_Start = {
-	"mge/UI/Dialogue/cat1.png",
-	"mge/UI/Dialogue/cat2.png",
-	"mge/UI/Dialogue/cat1.png",
-	"mge/UI/Dialogue/cat2.png",
-	"mge/UI/Dialogue/cat1.png"
+	"mge/UI/Dialogue/Lvl5_start_1.png",
+	"mge/UI/Dialogue/Lvl5_start_2.png"
 }
 
 Level5_Exit = {
-	"mge/UI/Dialogue/hamster1.png",
-	"mge/UI/Dialogue/hamster2.png",
-	"mge/UI/Dialogue/hamster1.png",
-	"mge/UI/Dialogue/hamster2.png",
-    "mge/UI/Dialogue/hamster1.png"
+	"mge/UI/Dialogue/Lvl5_end_1.png"
 }
 
 --Level 6
 Level6_Start = {
-	"mge/UI/Dialogue/cat1.png",
-	"mge/UI/Dialogue/cat2.png",
-	"mge/UI/Dialogue/cat1.png",
-	"mge/UI/Dialogue/cat2.png",
-    "mge/UI/Dialogue/cat1.png",
-    "mge/UI/Dialogue/cat2.png"
+	"mge/UI/Dialogue/Lvl6_start_1.png",
+	"mge/UI/Dialogue/Lvl6_start_2.png"
 }
 
 Level6_Exit = {
-	"mge/UI/Dialogue/hamster1.png",
-	"mge/UI/Dialogue/hamster2.png",
-	"mge/UI/Dialogue/hamster1.png",
-	"mge/UI/Dialogue/hamster2.png",
-    "mge/UI/Dialogue/hamster1.png",
-    "mge/UI/Dialogue/hamster2.png"
+	"mge/UI/Dialogue/Lvl6_end_1.png",
+	"mge/UI/Dialogue/Lvl6_end_2.png"
 }
